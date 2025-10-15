bf95978b7280f60d47c8509dd0f36e69eadd7621
raimusthafa <raiimusthafa@gmail.com>
2025-10-16 00:05:25 +0700

    if else (tarif parkir)

--- week5/parkir.java
+++ week5/parkir.java
@@ -13,29 +13,36 @@ public class parkir {
         int jam = input.nextInt();
 
         int biaya = 0;
+        String denda = " ";
 
         if (jenis.equalsIgnoreCase("motor")) {
             if (jam <= 0) {
                 biaya = 0;
             } else if (jam == 1) {
                 biaya = 3000;
-            } else {
-                biaya = 3000 + (jam - 1) * 2000;
+            } else if (jam > 1 && jam <= 24) {
+                biaya = 30000 + 3000 + (jam - 1) * 2000;
+            } else if (jam > 24) {
+                biaya = 30000 + 3000 + (jam - 1) * 2000 + ((jam - 24) / 24) * 30000;
+                denda = " (termasuk denda)";
             }
         } else if (jenis.equalsIgnoreCase("mobil")) {
             if (jam <= 0) {
                 biaya = 0;
             } else if (jam == 1) {
                 biaya = 5000;
-            } else {
+            } else if (jam > 1 && jam <= 24) {
                 biaya = 5000 + (jam - 1) * 3000;
+            } else if (jam > 24) {
+                biaya = 50000 + 5000 + (jam - 1) * 3000;
+                denda = " (termasuk denda)"; 
             }
         } else {
             System.out.print("Input tidak valid!");
             return;
         }
         
-        System.out.print("Total biaya parkir: Rp" + biaya);
+        System.out.print("Total biaya parkir: Rp" + biaya + denda);
 
     }
 }
